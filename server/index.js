740842a4e2a72a7289226ad74780fa585fe525fc
Christian Zenaty <cazenaty@yahoo.com>
2017-12-18 12:25:36 -0800

    clientside working

--- server/index.js
+++ server/index.js
@@ -5,22 +5,38 @@ const WebSocket = require("ws");
 
 const app = express();
 
-const server = http.createServer(app);
-const ws = new WebSocket("wss://ws-feed.gdax.com");
+//Recreate socket.io Chat App with WS
 
-ws.on("open", () => {
-  const heartbeat = {
-    type: "subscribe",
-    channels: [{ name: "heartbeat", product_ids: ["ETH-EUR"] }]
-  };
-  ws.send(JSON.stringify(heartbeat));
-});
+const GDAX_ENDPOINT = "wss://ws-feed.gdax.com";
+const server = http.createServer(app);
+// const ws = new WebSocket(GDAX_ENDPOINT);
+const wsServer = new WebSocket.Server({ port: 8000 });
 
 app.get("/", (req, res) => {
-  ws.on("message", data => {
-    console.log(data);
+  res.sendFile(__dirname + "/index.html");
+});
+
+wsServer.on("connection", ws => {
+  // console.log("Serverside:", ws);
+  ws.on("message", msg => {
+    console.log("incoming msg: %m", msg);
+    ws.send(JSON.stringify(msg));
   });
 });
+
+// const heartbeat = {
+//   type: "subscribe",
+//   product_ids: ["BTC-USD", "ETH-USD"],
+//   channels: ["heartbeat", "ticker"]
+// };
+
+// ws.on("open", () => {
+//   ws.send(JSON.stringify(heartbeat));
+// });
+
+// ws.on("message", data => {
+//   console.log("-", data);
+// });
 
 server.listen(8080, () => {
   console.log("Listening on %d", server.address().port);
