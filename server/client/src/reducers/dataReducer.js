96160573e12f33df5b7ef68d03995b21efc32440
Christian Zenaty <cazenaty@yahoo.com>
2018-01-11 22:45:19 -0800

    Redux fixed and DONE!

--- server/client/src/reducers/dataReducer.js
+++ server/client/src/reducers/dataReducer.js
@@ -22,7 +22,7 @@
 const dataReducer = (state = {}, action) => {
   switch (action.type) {
     case "WEBSOCKET_DATA":
-      return Object.assign({}, state, action.payload);
+      return Object.assign({}, state, action.data);
     default:
       return state;
   }
